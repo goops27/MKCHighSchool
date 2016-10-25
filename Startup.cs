b108639f30182433dab5b13a4f1179fc75eb152c
Gopal Pradhan <goops27@gmail.com>
2016-10-25 10:19:23 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MKCWebApplication.Startup))]
+namespace MKCWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
